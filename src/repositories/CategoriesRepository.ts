972cccd405fc1f952f8be53630177eb384fcdb77
Yves de Sá <yvesdsb98@gmail.com>
2021-12-10 02:35:03 -0300

    Created the functions of the category entity.

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,42 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO{
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category [];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO ): Category {
+    const categoty = new Category();
+
+    Object.assign(categoty, {
+      name,
+      description,
+      create_at: new Date()
+    });
+  
+    this.categories.push(categoty);
+
+    return categoty;
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category | undefined {
+    const category = this.categories.find(
+      (category) => category.name === name
+    );
+
+    return category;
+  }
+}
+
+export { CategoriesRepository }
