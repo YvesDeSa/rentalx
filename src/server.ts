0f311b48c77e2c79b56a71cc667ef9a609b2f839
Yves de Sá <yvesdsb98@gmail.com>
2021-12-14 15:45:19 -0300

    Starting api documentation with Swagger.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import swaggerUi  from "swagger-ui-express";
+
 import { routers } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(routers);
 
 app.listen(3333, () => {
-  console.log("🚀 Server is running...")
+  console.log("🚀 Server is running in port 3333")
 });
